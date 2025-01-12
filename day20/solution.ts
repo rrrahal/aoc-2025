f7dbe9bf0be52f7226143302c511b024b0b45709
Rafael Rahal <rrrahal@gmail.com>
2025-01-12 11:18:09 -0300

    day20 part1

--- day20/solution.ts
+++ day20/solution.ts
@@ -1,7 +1,7 @@
 import { getInputLineByLine } from "../utils/input";
 
 export const part1 = async () => {
-  const input: string[] = await getInputLineByLine("example.txt");
+  const input: string[] = await getInputLineByLine("input.txt");
   const grid = input.map((l) => Array.from(l));
   const startingPos: number[] = [];
   const endPos: number[] = [];
@@ -17,31 +17,42 @@ export const part1 = async () => {
     }
   }
 
-  // const normalValue = bfs(startingPos, endPos, grid, -1);
-  // const cheats: number[] = [];
-  // for (let i = 0; i < normalValue; i++) {
-  //   cheats.push(bfs(startingPos, endPos, grid, i));
-  // }
+  const raceTrackWay = bfs(startingPos, endPos, grid);
+  const distances: Map<string, number> = new Map();
+  raceTrackWay.forEach((pos, index) => {
+    distances.set(`${pos}`, index);
+  });
+  const cheats = raceTrackWay.map((pos) => cheat(pos, 2, grid));
+  const saved = cheats
+    .map((c, i) => timeSaved(c, raceTrackWay[i], distances, 2))
+    .flat()
+    .filter((n) => n > 0);
 
-  const test = bfs(startingPos, endPos, grid, 20);
-  console.log(test);
-  // console.log(cheats);
+  return saved.filter((t) => t >= 100).length;
 };
 
-const bfs = (
-  start: number[],
-  end: number[],
-  grid: string[][],
-  cheatDist: number,
+const timeSaved = (
+  finalPos: number[][],
+  originalPos: number[],
+  distances: Map<string, number>,
+  cheatSteps: number,
 ) => {
+  return finalPos.map((p) => {
+    const original = distances.get(`${originalPos}`)! + cheatSteps;
+    const saved = distances.get(`${p}`)!;
+    return saved - original;
+  });
+};
+
+const bfs = (start: number[], end: number[], grid: string[][]) => {
   const seen: Set<string> = new Set();
   const queue: [number[], number][] = [[start, 0]];
-  const results: number[] = [];
+  const results: number[][] = [];
 
   while (queue.length) {
     const [node, dist] = queue.shift()!;
     if (node[0] === end[0] && node[1] === end[1]) {
-      results.push(dist);
+      return results;
     }
 
     if (seen.has(`${node[0]},${node[1]}`)) {
@@ -49,11 +60,9 @@ const bfs = (
     }
 
     seen.add(`${node[0]},${node[1]}`);
-    if (cheatDist === dist) {
-      printGrid(grid, node);
-    }
+    results.push(node);
     const neighbors = getNeighbors(node[0], node[1]).filter((n) =>
-      isNode(n[0], n[1], grid, cheatDist === dist),
+      isNode(n[0], n[1], grid, false),
     );
 
     neighbors.forEach((n) => {
@@ -64,10 +73,32 @@ const bfs = (
   return results;
 };
 
-const printGrid = (grid: string[][], pos: number[]) => {
-  const copy = [...grid];
-  copy[pos[0]][pos[1]] = "!";
-  copy.map((l) => console.log(l.join("")));
+const cheat = (start: number[], cheatSteps: number, grid: string[][]) => {
+  // given a start position and how many cheat steps, returns an array of all possible end positions
+  let q = [start];
+  const seen: Set<string> = new Set();
+  while (cheatSteps > 1) {
+    q.forEach((n) => {
+      seen.add(`${n[0]},${n[1]}`);
+    });
+    q = q
+      .map((n) =>
+        getNeighbors(n[0], n[1])
+          .filter((node) => getWall(node[0], node[1], grid))
+          .filter((ne) => !seen.has(`${ne[0]},${ne[1]}`)),
+      )
+      .flat();
+    cheatSteps--;
+  }
+
+  return q
+    .map((n) =>
+      getNeighbors(n[0], n[1]).filter(
+        (node) =>
+          isNode(node[0], node[1], grid) && !seen.has(`${node[0]},${node[1]}`),
+      ),
+    )
+    .flat();
 };
 
 const isNode = (
@@ -89,6 +120,17 @@ const isNode = (
     return false;
   }
 };
+const getWall = (i: number, j: number, grid: string[][]) => {
+  try {
+    const el = grid[i][j];
+    if (el === undefined) {
+      return false;
+    }
+    return el === "#";
+  } catch (e) {
+    return false;
+  }
+};
 
 const getNeighbors = (i: number, j: number): number[][] => {
   return [
@@ -99,4 +141,32 @@ const getNeighbors = (i: number, j: number): number[][] => {
   ];
 };
 
-console.log(await part1());
+export const part2 = async () => {
+  const input: string[] = await getInputLineByLine("example.txt");
+  const grid = input.map((l) => Array.from(l));
+  const startingPos: number[] = [];
+  const endPos: number[] = [];
+  for (let i = 0; i < input.length; i++) {
+    for (let j = 0; j < input[0].length; j++) {
+      const element = input[i][j];
+      if (element === "S") {
+        startingPos.push(i, j);
+      }
+      if (element === "E") {
+        endPos.push(i, j);
+      }
+    }
+  }
+
+  const raceTrackWay = bfs(startingPos, endPos, grid);
+  const distances: Map<string, number> = new Map();
+  raceTrackWay.forEach((pos, index) => {
+    distances.set(`${pos}`, index);
+  });
+  // given all the points in the path
+  // find all the reacheble point within 20s and their distance
+  // calculate how many time we saved for each of them
+};
+
+// console.log(await part1());
+console.log(await part2());
