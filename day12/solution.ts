c7509f691b396c1fe08c71bed63152c1fcc7eceb
Rafael Rahal <rrrahal@gmail.com>
2025-01-20 10:22:19 -0300

    finish day 12

--- day12/solution.ts
+++ day12/solution.ts
@@ -20,32 +20,91 @@ export const part1 = async () => {
     }
   }
 
-  console.log(regions);
+  console.log(regions[0]);
   return regions.reduce((acc, region) => {
     return acc + region.perimeter * region.areas.length;
   }, 0);
 };
 
 export const part2 = async () => {
-  const text: string[] = await getInputLineByLine("example.txt");
+  // I need to count the areas
+  // and the vertices, because vertices === sides
+  const text: string[] = await getInputLineByLine("input.txt");
   const grid = text.map((line) => Array.from(line));
 
   const inRegion = new Set<string>();
-  const regions: any[] = [];
+  const regions: Region[] = [];
 
   for (let i = 0; i < grid.length; i++) {
     for (let j = 0; j < grid[0].length; j++) {
       if (!inRegion.has(`${i},${j}`)) {
-        regions.push(getRegion2(i, j, inRegion, grid));
+        regions.push(getRegion(i, j, inRegion, grid));
       }
     }
   }
 
   return regions.reduce((acc, r) => {
-    const edges = findEdges(r.perimeter, r.areas);
-    console.log(edges);
-    return acc + r.areas.length * edges.size;
+    return acc + r.areas.length * countSides(r.areas);
   }, 0);
+};
+
+const countSides = (area: number[][]) => {
+  const up = new Set();
+  const down = new Set();
+  const right = new Set();
+  const left = new Set();
+  const points = new Set(area.map((a) => JSON.stringify(a)));
+
+  area.forEach(([i, j]) => {
+    const key = JSON.stringify([i, j]);
+    if (!points.has(JSON.stringify([i - 1, j]))) {
+      up.add(key);
+    }
+    if (!points.has(JSON.stringify([i + 1, j]))) {
+      down.add(key);
+    }
+    if (!points.has(JSON.stringify([i, j - 1]))) {
+      left.add(key);
+    }
+    if (!points.has(JSON.stringify([i, j + 1]))) {
+      right.add(key);
+    }
+  });
+  let count = 0;
+
+  Array.from(up).forEach((item: number[]) => {
+    if (left.has(item)) {
+      count = count + 1;
+    }
+    if (right.has(item)) {
+      count = count + 1;
+    }
+    const [i, j] = JSON.parse(item);
+    if (right.has(JSON.stringify([i - 1, j - 1])) && !left.has(item)) {
+      count = count + 1;
+    }
+    if (left.has(JSON.stringify([i - 1, j + 1])) && !right.has(item)) {
+      count = count + 1;
+    }
+  });
+
+  Array.from(down).forEach((item) => {
+    if (left.has(item)) {
+      count = count + 1;
+    }
+    if (right.has(item)) {
+      count = count + 1;
+    }
+    const [i, j] = JSON.parse(item);
+    if (right.has(JSON.stringify([i + 1, j - 1])) && !left.has(item)) {
+      count = count + 1;
+    }
+    if (left.has(JSON.stringify([i + 1, j + 1])) && !right.has(item)) {
+      count = count + 1;
+    }
+  });
+
+  return count;
 };
 
 const getRegion = (
@@ -85,74 +144,6 @@ const getRegion = (
   return region;
 };
 
-const getRegion2 = (
-  i: number,
-  j: number,
-  inRegion: Set<string>,
-  grid: string[][],
-) => {
-  const currentRegion = grid[i][j];
-  const perimeter = [] as number[][];
-  const region = {
-    perimeter: [] as number[][],
-    areas: [] as number[][],
-  };
-  inRegion.add(`${i},${j}`);
-  region.areas.push([i, j]);
-  const n = getNeighbors2(i, j, currentRegion, grid, inRegion, perimeter);
-  while (n.length > 0) {
-    const node = n.shift();
-    if (!node || inRegion.has(`${node[0]},${node[1]}`)) {
-      continue;
-    }
-    inRegion.add(`${node[0]},${node[1]}`);
-    region.areas.push(node);
-    const neighbors = getNeighbors2(
-      node[0],
-      node[1],
-      currentRegion,
-      grid,
-      inRegion,
-      perimeter,
-    );
-    n.push(...neighbors);
-  }
-
-  region.perimeter = perimeter;
-  return region;
-};
-const getNeighbors2 = (
-  i: number,
-  j: number,
-  currentRegion: string,
-  grid: string[][],
-  inRegion: Set<string>,
-  perimeter: number[][],
-) => {
-  const up = [i + 1, j];
-  const down = [i - 1, j];
-  const right = [i, j + 1];
-  const left = [i, j - 1];
-
-  return [up, down, right, left].filter((el) => {
-    try {
-      if (
-        grid[el[0]][el[1]] === currentRegion &&
-        !inRegion.has(`${el[0]},${el[1]}`)
-      ) {
-        return true;
-      }
-      if (grid[el[0]][el[1]] !== currentRegion) {
-        perimeter.push([el[0], el[1]]);
-      }
-      return false;
-    } catch (e) {
-      perimeter.push([el[0], el[1]]);
-      return false;
-    }
-  });
-};
-
 const getNeighbors = (
   i: number,
   j: number,
@@ -183,56 +174,6 @@ const getNeighbors = (
       return false;
     }
   });
-};
-
-const findEdges = (perimeter: number[][], region: number[][]) => {
-  const edges = new Set<string>();
-  const inPerimeter = new Set<string>();
-  const inRegion = new Set<string>();
-
-  perimeter.forEach((p) => {
-    inPerimeter.add(`${p[0]},${p[1]}`);
-  });
-
-  region.forEach((r) => {
-    inRegion.add(`${r[0]},${r[1]}`);
-  });
-
-  perimeter.forEach((p) => {
-    const isTop = inRegion.has(`${p[0] - 1},${p[1]}`);
-    const isBottom = inRegion.has(`${p[0] + 1},${p[1]}`);
-    const isRight = inRegion.has(`${p[0]},${p[1] + 1}`);
-    const isLeft = inRegion.has(`${p[0]},${p[1] - 1}`);
-
-    if ((isTop && (isLeft || isRight)) || (isBottom && (isLeft || isRight))) {
-      if ([isRight, isLeft, isBottom, isTop].filter((t) => t).length > 2) {
-        edges.add(`${p[0]},${p[1]}`);
-        edges.add(`${p[0]},${p[1]}-`);
-        return;
-      }
-      edges.add(`${p[0]},${p[1]}`);
-      return;
-    }
-
-    if (isTop || isBottom) {
-      if (!inPerimeter.has(`${p[0]},${p[1] - 1}`)) {
-        edges.add(`${p[0]},${p[1] - 1}`);
-      }
-      if (!inPerimeter.has(`${p[0]},${p[1] + 1}`)) {
-        edges.add(`${p[0]},${p[1] + 1}`);
-      }
-    }
-    if (isRight || isLeft) {
-      if (!inPerimeter.has(`${p[0] + 1},${p[1]}`)) {
-        edges.add(`${p[0] + 1},${p[1]}`);
-      }
-      if (!inPerimeter.has(`${p[0] - 1},${p[1]}`)) {
-        edges.add(`${p[0] - 1},${p[1]}`);
-      }
-    }
-  });
-
-  return edges;
 };
 
 // console.log(await part1());
